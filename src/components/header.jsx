46dede44c73b008b10cec10e44d9737241718aa7
Sanan <budledyayev@bk.ru>
2024-08-21 14:57:45 +0300

    feat(home-page): game list display

---
+++ src/components/header.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './header.css'
+import { CartBlock } from './cart-block/cart-block'
+import { Link } from 'react-router-dom'
+
+export const Header = () => {
+  return (
+    <div className='header'>
+     <div className="wrapper">
+      <Link to='/' className='header__store-title'>
+      Game Store
+      </Link>
+     </div>
+     <div className='wrapper header__cart-btn-wrapper'>
+      <CartBlock/>
+     </div>
+    </div>
+  )
+
+}
+
+
+ 
